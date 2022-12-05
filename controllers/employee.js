e37f08f4f9822838e56eb0daa49a3b369fa08b0f
CT08\CT08 <pcthanh0802@gmail.com>
2022-12-05 12:17:32 +0700

    add get account by username

--- controllers/employee.js
+++ controllers/employee.js
@@ -3,6 +3,18 @@ const Employee = require('../models/user');
 async function getEmployeeByRole(req, res) {
     try {
         const result = await Employee.getEmployeeByRole(req.body.role)
+        res.send(result);
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+async function getEmployeeByUsername(req, res) {
+    try {
+        const result = await Employee.getEmployeeByUsername(req.params.username);
+        if(!result) return res.status(404).send("User not found");
+        res.send(result);
     } catch(err) {
         console.log(err);
         res.sendStatus(500);
@@ -10,5 +22,6 @@ async function getEmployeeByRole(req, res) {
 }
 
 module.exports = {
-    getEmployeeByRole
+    getEmployeeByRole,
+    getEmployeeByUsername
 }
--- routes/employee.js
+++ routes/employee.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const employeeController = require('../controllers/employee');
 
 router.post('/role', employeeController.getEmployeeByRole);
+router.get('/info/:username', employeeController.getEmployeeByUsername);
 
 module.exports = router;
